7a9d15bff6182e787c3e9669f008a1ed54835933
Alex Stewart <astewart1510@gmail.com>
2021-09-10 16:57:53 +0200

    feat: page parser function

---
+++ page_parser.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+def parse_page(response):
+    article_titles_list = []
+    article_titles = response.select(".link-no-underline")
+    for title in article_titles:
+        article_titles_list.append(title.text)
+
+    article_date_pubs = response.select(".metadata")
+    article_date_pub_list = []
+    for date in article_date_pubs:
+        s = date.text
+        target_date = s.split("(")[1].split(")")[0]
+        article_date_pub_list.append(target_date)
+
+    article_urls = response.select(".pdfLink")
+    article_url_list = []
+    for url in article_urls:
+        s = url["href"]
+        target_url = s.split("?")[0]
+        full_url = "https://www.jstor.org/" + target_url
+        article_url_list.append(full_url)
+
+    articles = pd.DataFrame(
+        list(zip(article_titles_list, article_date_pub_list, article_url_list)),
+        columns=["Title", "Date Published", "URL"],
+    )
+    return articles
+
+
+# To test - downloaded the <html> element off the page below and run the following
+# https://www.jstor.org/action/doBasicSearch?Query=pt%3A%28Econometrica%29
+
+# soup = BeautifulSoup(open('/Users/myhome/Documents/work/FinLab/Fintech Student Project/Info scrape experiment/JSTOR_ Search Results.html'), 'html.parser')
+# articles = parse_page(soup)
+# print(articles)
