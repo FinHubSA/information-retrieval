00f920a1a710e7ce4ee294b2441cd30b312c4e40
MBRGA <thatdeaddude@gmail.com>
2021-09-15 20:03:42 +0200

    Added wait time and csrftoken refresh

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,3 +1,4 @@
+from math import log
 from bs4 import BeautifulSoup
 import requests 
 from http.cookies import SimpleCookie
@@ -6,24 +7,26 @@ import bibtexparser
 import ast
 from typing import Callable
 from pathlib import Path
+from random import random
+from time import sleep
 
 class JstorArticle:
     """This class encapsulates the metadata and actual article downloaded from JSTOR
 
     Args:
-        * meta_dict (dict) : Dictionary of arbitrary metadata key-value pairs
+        * meta_dict (str) : String representation of JSON object with arbitrary metadata key-value pairs
         * pdf (bytes) : Raw bytes of downloaded pdf
 
     Attributes: 
-        metadata_dict (dict) : Direct access to metadata dictionary
+        metadata_json (str) : Direct access to metadata JSON object
     """
-    _pdf_blob = None
+    _pdf_blob : bytes = None
 
-    metadata_dict = None
+    metadata_json : str = None
 
-    def __init__(self, meta_dict: dict, pdf: bytes) -> None:
+    def __init__(self, meta_json: str, pdf: bytes) -> None:
         self._pdf_blob = pdf
-        self.metadata_dict = meta_dict
+        self.metadata_json = meta_json
 
     def save_pdf(self, path: Path) -> None:
         """Saves the JstorArticle's pdf data to the given Path
@@ -32,7 +35,7 @@ class JstorArticle:
             * path (Path): Path object providing location for data to be saved to
         """
 
-        with path.open(OUT_FILE, 'xb') as p:
+        with path.open(path, 'xb') as p:
             p.write(self._pdf_blob)
 
 
@@ -46,6 +49,13 @@ class JstorScraper:
         * preview_path (str, optional) : URL relative to base_url for article description/preview. Defaults to `/stable/`.
         * pdf_path (str, optional) : URL relative to base_url for pdf download. Defaults to `/stable/pdf/`.
         * metadata_path (str, optional) : URL relative to base_url for JSON metadata API. Defaults to `/stable/content-metadata/`.
+        * mean_request_delay_s (int, optional) : Mean wait time between requests. 
+            These will follow an exponential distribution with mean equal to this value. Defaults to 15.
+        * log_level (int, optional) : Controls the amount of output printed. Defaults to 1. Options are
+            - 0: No output 
+            - 1: Status updates
+            - 2: Full request logging (not yet implemented)
+            - 3: Verbose logging (not yet implemented)
     """
 
     _session : requests.Session = None
@@ -60,13 +70,19 @@ class JstorScraper:
 
     _meta_path : str = None
 
+    _mean_wait_time_s : int = 15
+
+    _log_level : int = 1
+
     def __init__(self, 
                  web_session: requests.Session, 
                  rewrite_rule: Callable[[str], str] = None, 
                  base_url: str = 'https://www.jstor.org',
                  preview_path: str = '/stable/',
                  pdf_path: str = '/stable/pdf/',
-                 metadata_path: str = '/stable/content-metadata/') -> None:
+                 metadata_path: str = '/stable/content-metadata/',
+                 mean_request_delay_s: int = 15,
+                 log_level = 1) -> None:
 
         # Populate private attributes:                 
 
@@ -80,11 +96,24 @@ class JstorScraper:
 
         self._meta_path = metadata_path
 
+        self._mean_wait_time_s = mean_request_delay_s
+
+        self._log_level = log_level
+
         # If there is no rewrite rule just use identity function:
         if rewrite_rule == None:
             self._rewrite_rule = lambda m : m
         else:
             self._rewrite_rule = rewrite_rule
+
+    def _wait_before_request(self):
+
+        n_seconds = -self._mean_wait_time_s * log(random())
+
+        if self._log_level > 0:
+            print(f'Waiting {n_seconds:.1f}s before next request', end = '\r')
+
+        sleep(n_seconds)
 
     # Loads JSTOR page and finds link to download PDF
     def get_payload_data(self, document_id: int) -> JstorArticle:
@@ -101,60 +130,83 @@ class JstorScraper:
 
         """
 
-        view_uri = f'{self._base_url}{self._prev_path}{str(document_id)}'
-
-        print(self._rewrite_rule(view_uri))
-        print(self._session.headers)
+        view_uri = self._rewrite_rule(f'{self._base_url}{self._prev_path}{document_id}')
 
         # Send the request
-        page_request = self._session.get(self._rewrite_rule(view_uri))
+        self._wait_before_request()
+
+        if self._log_level > 0:
+            print(f'Performing GET request for article landing page at {view_uri}', end = '\r')
+        page_request = self._session.get(view_uri)
 
         # View response
         if page_request.status_code != 200:
             print(page_request.text)
             raise ValueError(f'Received response code {page_request.status_code}')
 
-        # Build DOM model
+        # Build DOM model and find CSRF token
         view_page_soup = BeautifulSoup(page_request.content, 'html.parser')
 
-        # Most of the JSTOR page is built dynamically, so there's nothing to scrape directly :'(
-        # Try to get document metadata from Google Analytics script block. 
-        # TODO: consider adding any missing fields from elsewhere?
-        try:
-            jstor_metadata_script = view_page_soup.head.find('script', attrs={'data-analytics-provider':'ga'}).string
+        cookie_div = view_page_soup.find('div', attrs = {'id': 'csrfTokenCookie', 'class': 'hide'})
 
-            jstor_metadata_match = re.search(r'gaData.content = (?P<dict>{[^}]+});', jstor_metadata_script)
+        csrf_token = cookie_div.find('input', attrs = {'name': 'csrfmiddlewaretoken'})['value']
+        
+        self._session.cookies.set('csrftoken', 
+                                  csrf_token, 
+                                  domain = self._rewrite_rule(self._base_url), 
+                                  path = '/',
+                                  discard = True)
 
-            jstor_data_dict = ast.literal_eval(jstor_metadata_match.group('dict'))
-        except TypeError as exc:
-            raise ValueError('Unable to get document metadata from JSTOR response') from exc
+        # Download article metadata
+        # We don't need to wait for this because the browser will load it without delay anyway
+        meta_uri = self._rewrite_rule(f'{self._base_url}{self._meta_path}{document_id}')
+
+        if self._log_level > 0:
+            print(f'Performing GET request for article metadata at {meta_uri}', end = '\r')
+
+        meta_request = self._session.get(meta_uri)
+
+        jstor_metadata = meta_request.text
 
         # Now try download the pdf
-        pdf_uri = f'{self._base_url}{self._pdf_path}{str(document_id)}.pdf'
+        pdf_uri = self._rewrite_rule(f'{self._base_url}{self._pdf_path}{document_id}.pdf')
 
-        pdf_request = self._session.get(self._rewrite_rule(pdf_uri))
+        self._wait_before_request()
+
+        if self._log_level > 0:
+            print(f'Performing GET request for article pdf at {pdf_uri}', end = '\r')
+
+        pdf_request = self._session.get(pdf_uri)
 
         # JSTOR may ask us to request terms and conditions - have to send a new request accepting them
         if re.match(r'text/html', pdf_request.headers['content-type']):
-            print('wompwomp')
 
             pdf_page_soup = BeautifulSoup(pdf_request.content, 'html.parser')
 
             accept_form = pdf_page_soup.find('form', attrs = {'method': 'POST', 'action': re.compile(r'/tc/verify')})
 
             csrf_token = accept_form.find('input', attrs = {'name': 'csrfmiddlewaretoken'})['value']
+        
+            self._session.cookies.set('csrftoken', 
+                                    csrf_token, 
+                                    domain = self._rewrite_rule(self._base_url), 
+                                    path = '/',
+                                    discard = True)
 
             pdf_request_payload = {'csrfmiddlewaretoken' : csrf_token}
 
             # Update URI according to action from prior request
-            pdf_uri = f'{self._base_url}{accept_form["action"]}'
+            pdf_uri = self._rewrite_rule(f'{self._base_url}{accept_form["action"]}')
 
-            print(pdf_uri)
+            if self._log_level > 0:
+                print(f'JSTOR returned T&C page. Performing POST request for article pdf at {pdf_uri}', end = '\r')
 
-            pdf_request = self._session.post(self._rewrite_rule(pdf_uri), data = pdf_request_payload)
+            self._wait_before_request()
+
+            pdf_request = self._session.post(pdf_uri, data = pdf_request_payload)
 
         # Do a final check that we have apparently received a pdf as expected.
         if pdf_request.headers['content-type'] != 'application/pdf':
-            raise ValueError('JSTOR did not return a pdf when expected - got response MIME content type of ' + pdf_request.headers['content-type'])
+            raise ValueError(f'JSTOR did not return a pdf when expected - got response MIME content type of {pdf_request.headers["content-type"]}')
 
-        return JstorArticle(jstor_data_dict, pdf_request.content)
+        return JstorArticle(jstor_metadata, pdf_request.content)
