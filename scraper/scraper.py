ab04232834f9cb80b8a88e63b3a694582016963d
Rachel Swallow <rswallow987@gmail.com>
2021-10-07 20:26:04 +0200

    fix: attempted to try add page parser to scraper but it's not working

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,6 @@
 from selenium.common.exceptions import ErrorInResponseException, TimeoutException
 import re
+import pandas as pd 
 
 from math import log
 from random import random
@@ -8,10 +9,11 @@ from typing import Callable
 from pathlib import Path
 from http.cookies import SimpleCookie
 
-from page_parser import parse_search_page
+#from metascrape import parse_search_page
 
 import requests 
 from bs4 import BeautifulSoup
+import lxml
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -130,6 +132,52 @@ class JstorScraper:
             print(f'Waiting {n_seconds:.1f}s before next request', end = '\r')
 
         sleep(n_seconds)
+        
+    def parse_search_page(response):
+        article_titles_list = []
+        article_titles = response.select('.link-no-underline')
+        for title in article_titles:
+            article_titles_list.append(title.text)
+            
+        article_author_list = []
+        article_author = response.select('.contrib')
+        for author in article_author:
+            article_author_list.append(author.text)
+        
+        journal = response.select('.metadata') 
+        article_journal_name_list = []
+        article_volume_list = []
+        article_journal_date_list = []
+        article_pages_in_journal_list = []
+        for article in journal:
+            s = article.text
+            #get journal date published
+            target_date = s.split("(")[1].split(")")[0]
+            article_journal_date_list.append(target_date)
+            
+            #get article journal full name
+            target_name = s.split(",")[0]
+            article_journal_name_list.append(target_name)
+            
+            #get article journal volume
+            target_volume = s.split(",")[1].split("(")[0]
+            article_volume_list.append(target_volume)
+            
+            #get article pages in journal
+            target_pages = str(s.split("),")[1:])
+            article_pages_in_journal_list.append(target_pages[2:-2])
+            
+            
+        article_urls = response.select('.pdfLink')
+        article_url_list = []
+        for url in article_urls:
+            s = url['href']
+            target_url = s.split("?")[0]
+            full_url = 'https://www.jstor.org/' + target_url
+            article_url_list.append(full_url)  
+            
+        articles = pd.DataFrame(list(zip(article_titles_list, article_author_list, article_journal_name_list, article_volume_list ,article_journal_date_list , article_pages_in_journal_list ,  article_url_list)), columns=['Title',"Author",'Journal', 'Journal Volume','Journal Date Published','Article Pages', 'URL'])
+        return articles
     
         
     def get_search_results(self, journal_name: str, request_timeout: int=10):
@@ -176,9 +224,9 @@ class JstorScraper:
         search_bar.send_keys(journal) 
         search_button.click()
         self._wait_before_request()
-        soup = BeautifulSoup(self._driver.page_source)
+        soup = BeautifulSoup(self._driver.page_source, 'html.parser')
         self._wait_before_request()
-        articles = parse_search_page(soup)
+        articles = self.parse_search_page(soup)
         
         return articles
         
